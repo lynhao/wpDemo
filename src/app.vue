61f6d51731e6e7a0effa04019351883d036f5f8b
linhao <linhao@foresee.com.cn>
2018-04-02 23:56:34 +0800

    原始工程

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">{{text}}</div>
+</template>
+<script type="text/javascript">
+  export default {
+  	data() {
+  	  return {
+  	  	text: 'text'
+  	  }
+  	}
+  }
+</script>
+<style scoped>
+	
+</style>
